c2c90b57bb72c6926d8543755634a0ae770eab99
Adam Rankin <adamr14@vt.edu>
2020-03-25 12:28:37 -0400

    Take out declarations

--- repository.py
+++ repository.py
@@ -19,46 +19,6 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(
     host='localhost'))
 channel = connection.channel()
 
-# Exchanges
-channel.exchange_declare(exchange='Squires')
-channel.exchange_declare(exchange='Goodwin')
-channel.exchange_declare(exchange='Library')
-
-# Message Queues
-channel.queue_declare(queue='Food')
-channel.queue_declare(queue='Meetings')
-channel.queue_declare(queue='Rooms')
-channel.queue_declare(queue='Classrooms')
-channel.queue_declare(queue='Auditorium')
-channel.queue_declare(queue='Noise')
-channel.queue_declare(queue='Seating')
-channel.queue_declare(queue='Wishes')
-
-# Bind queues to Exchanges
-channel.queue_bind(exchange='Squires',
-                   queue='Food',
-                   routing_key='Food')
-channel.queue_bind(exchange='Squires',
-                   queue='Meetings',
-                   routing_key='Meetings')
-channel.queue_bind(exchange='Squires',
-                   queue='Rooms',
-                   routing_key='Rooms')
-channel.queue_bind(exchange='Goodwin',
-                   queue='Classrooms',
-                   routing_key='Classrooms')
-channel.queue_bind(exchange='Goodwin',
-                   queue='Auditorium',
-                   routing_key='Auditorium')
-channel.queue_bind(exchange='Library',
-                   queue='Noise',
-                   routing_key='Noise')
-channel.queue_bind(exchange='Library',
-                   queue='Seating',
-                   routing_key='Seating')
-channel.queue_bind(exchange='Library',
-                   queue='Wishes',
-                   routing_key='Wishes')
 
 
 
