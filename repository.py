d6147e20a1837d5ba668cb2a752e3959609dc13f
Adam Rankin <adamr14@vt.edu>
2020-03-25 13:47:05 -0400

    Final

--- repository.py
+++ repository.py
@@ -25,33 +25,34 @@ def callback(ch, method, properties, body):
     
 
 while 1:
-    #try:
-    # Parse input (look in helpers.py)
-    mongo_insert = helpers.parse_input(input("Please input your command:"))
-    
-    # print checkpoint 2
-    helpers.print_checkpoint(2, 'Store Command in MongoDB instance', mongo_insert)
-    
-    # insert to mongo database
-    db.utilization.insert(mongo_insert)
-    
-    # Do action
-    if mongo_insert['Action'] == 'p':
-        # produce RabbitMQ message
-        channel.basic_publish(exchange = mongo_insert['Place'],
-                              routing_key = mongo_insert['Subject'],
-                              body=mongo_insert['Message'])
-        helpers.print_checkpoint(3, 'Sent produce type message to rabbitMQ - Queue', mongo_insert['Subject'])
-    elif mongo_insert['Action'] == 'c':
-        # consume RabitMQ message
-        channel.basic_consume(callback,
-                              queue=mongo_insert['Subject'],
-                              no_ack=True)
-        helpers.print_checkpoint(3, 'Output from queue consumption via callback', '')
-        try:
-            channel.start_consuming()
-        except KeyboardInterrupt:
-            channel.stop_consuming()
-            
-    #except Exception as e:
-       #print(e)
+    try:
+        # Parse input (look in helpers.py)
+        mongo_insert = helpers.parse_input(input("Please input your command:"))
+        
+        # print checkpoint 2
+        helpers.print_checkpoint(2, 'Store Command in MongoDB instance', mongo_insert)
+        
+        # insert to mongo database
+        db.utilization.insert(mongo_insert)
+        
+        # Do action
+        if mongo_insert['Action'] == 'p':
+            # produce RabbitMQ message
+            channel.basic_publish(exchange = mongo_insert['Place'],
+                                  routing_key = mongo_insert['Subject'],
+                                  body=mongo_insert['Message'])
+            helpers.print_checkpoint(3, 'Sent produce type message to rabbitMQ - Queue', mongo_insert['Subject'])
+        elif mongo_insert['Action'] == 'c':
+            # consume RabitMQ message
+            channel.basic_consume(callback,
+                                  queue=mongo_insert['Subject'],
+                                  no_ack=True)
+            helpers.print_checkpoint(3, 'Output from queue consumption via callback', '')
+            try:
+                channel.start_consuming()
+            except KeyboardInterrupt:
+                channel.stop_consuming()
+    except KeyboardInterrupt:
+            print('Goodbye!')
+    except Exception as e:
+        print(e)
