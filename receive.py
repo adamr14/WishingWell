997b5aab2cf9d97c1be5f7803460031126264c2f
Adam Rankin <adamr14@vt.edu>
2020-04-02 09:29:43 -0400

    Work

--- receive.py
+++ receive.py
@@ -6,14 +6,16 @@ Created on Thu Apr  2 09:09:29 2020
 @author: adamrankin
 """
 import pika
+import pickle
 
 connection = pika.BlockingConnection(pika.ConnectionParameters( host='localhost'))
 channel = connection.channel() 
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='Adder')
 print (' [*] Waiting for messages. To exit press CTRL+C')
 
-def callback(ch, method, properties, body):       
-    print (" [x] Received %r" % (body,))
+def callback(ch, method, properties, body):
+    nums = pickle.loads(body)     
+    print (" [x] The sum of the list is" + str(sum(nums)))
     
-channel.basic_consume(callback,queue='hello',no_ack=True)
+channel.basic_consume(callback,queue='Adder',no_ack=True)
 channel.start_consuming()
